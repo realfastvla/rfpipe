543422fb8fa54614d8699dc2a7e3a3a6d456293d
Casey Law <caseyjlaw@gmail.com>
2016-07-31 22:34:19 -0600

    playing with numba grid function

--- rfpipe/search.py
+++ rfpipe/search.py
@@ -1,2 +1,25 @@
 # home of much of rtpipe.RT
 # state transformation stages should be in state
+
+
+
+# playing with numba gridding
+import numba
+import numpy as np
+
+npixx, npixy = 1024, 1024
+grid = np.zeros((npixx, npixy), dtype='complex64')
+
+@jit
+def grid_visibilities(data, grid, us, vs, freqs):
+    freq0 = freqs[-1]
+    nint, nbl, nchan, npol = data.shape
+
+    for i in xrange(nbl):
+        for freq in freqs:
+        u = int(us[i]*freq/freq0)
+        v = int(vs[i]*freq/freq0)
+
+        for m in xrange(nint):
+            for n xrange(npol):
+                grid[u, v] += data[m, i, j, n]
