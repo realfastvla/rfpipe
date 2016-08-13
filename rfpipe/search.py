804202c548474b06b7e89ecd2027ee0eb6d81e88
Casey Law <caseyjlaw@gmail.com>
2016-08-12 18:12:46 -0600

    playing with multicore and numba

--- rfpipe/search.py
+++ rfpipe/search.py
@@ -6,24 +6,15 @@ from __future__ import division  # for Python 2
 import numba, math
 from numba import cuda
 import numpy as np
+import threading
+import pyfftw
 #import cProfile
 
+##
+# profiling with mean time subtraction algorithm
+##
 
-@cuda.jit()
-def grid_visibilities(visdata, grid, us, vs, freqs):
-    freq0 = freqs[-1]
-    nint, nbl, nchan, npol = visdata.shape
-
-    for i in xrange(nbl):
-        for freq in freqs:
-            u = int(us[i]*freq/freq0)
-            v = int(vs[i]*freq/freq0)
-
-        for m in xrange(nint):
-            for n in xrange(npol):
-                grid[u, v] += visdata[m, i, j, n]
-
-def meantsub0(data):
+def meantsub_python(data):
     """ Reference pure python function for meantsub """
 
     nint, nbl, nchan, npol = data.shape
@@ -35,7 +26,7 @@ def meantsub0(data):
     
 
 @cuda.jit
-def meantsub(data):
+def meantsub_cuda(data):
     """ Calculate mean in time (ignoring zeros) and subtract in place """
 
     x,y,z = cuda.grid(3)
@@ -51,6 +42,111 @@ def meantsub(data):
         for i in range(nint):
             data[i, x, y, z] = data[i, x, y, z] - mean
 
+
+@numba.jit
+def meantsub_numba(data, nopython=True):
+    """ Calculate mean in time (ignoring zeros) and subtract in place """
+
+    nint, nbl, nchan, npol = data.shape
+    for i in range(nbl):
+        for j in range(nchan):
+            for k in range(npol):
+                ss = numba.complex64(0)
+                weight = 0
+                for l in range(nint):
+                    ss += data[l, i, j, k]
+                    if data[l, i, j, k] != 0j:
+                        weight = weight + 1
+                mean = ss/weight
+                for l in range(nint):
+                    data[l, i, j, k] -= mean
+
+
+@numba.guvectorize([(numba.complex64[:,:,:], numba.complex64[:,:,:])], '(m,n,o)->(m,n,o)', nopython=True, target='parallel')
+def meantsub_gu(data, res):
+    """ Vectorizes over time axis *at end*. Use np.moveaxis(0, 3) for input visbility array """ 
+
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            ss = numba.complex64(0)
+            weight = numba.int16(0)
+            for k in range(data.shape[2]):
+                ss += data[i,j,k]
+                if data[i,j,k] != 0j:
+                    weight = weight + 1
+                mean = ss/weight
+            for k in range(data.shape[0]):
+                res[i,j,k] = data[i,j,k] - mean
+    
+
+@cuda.jit()
+def grid_visibilities(visdata, grid, us, vs, freqs):
+    freq0 = freqs[-1]
+    nint, nbl, nchan, npol = visdata.shape
+
+    for i in xrange(nbl):
+        for freq in freqs:
+            u = int(us[i]*freq/freq0)
+            v = int(vs[i]*freq/freq0)
+
+        for m in xrange(nint):
+            for n in xrange(npol):
+                grid[u, v] += visdata[m, i, j, n]
+
+
+@numba.jit
+def fft1d_numba(data, res):
+    sh = data.shape
+
+    for i in range(sh[1]):
+        for j in range(sh[2]):
+            for k in range(sh[3]):
+                res[:, i, j, k] = np.fft.fft(data[:, i, j, k])
+
+
+def fft1d_python(data, res):
+    sh = data.shape
+
+    for i in range(sh[1]):
+        for j in range(sh[2]):
+            for k in range(sh[3]):
+                res[:, i, j, k] = np.fft.fft(data[:, i, j, k])
+
+@numba.jit(nogil=True)
+def npifft2(data, result):
+    """ Input for multithread wrapper. nogil seems to improve nothing """
+
+    result = np.fft.ifft2(data)
+
+
+@numba.jit(nogil=True)
+def fftw(data, result):
+    ifft2 = pyfftw.builders.ifft2(data, auto_align_input=True, auto_contiguous=True, planner_effort='FFTW_PATIENT')
+    result = ifft2(data)
+
+
+def make_multithread(inner_func, numthreads):
+    """
+    Run the given function inside *numthreads* threads, splitting its
+    arguments into equal-sized chunks.
+    """
+    def func_mt(data):
+        result = np.empty_like(data)
+
+        # Create argument tuples for each input chunk
+        length = len(data)
+        chunklen = length // numthreads
+        i0s = [chunklen*i for i in range(numthreads)]
+        i1s = [chunklen*(i+1) for i in range(numthreads)]
+        # Spawn one thread per chunk
+        threads = [threading.Thread(target=inner_func, args=(data[i0:i1], result[i0:i1])) for (i0,i1) in zip(i0s, i1s)]
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+        return result
+    return func_mt
+    
 
 @numba.vectorize([numba.bool_(numba.complex64)])
 def get_mask(x):
