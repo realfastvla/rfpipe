8477e5e6e2a63fec0642b6b6f13059e3b7fa19bf
Casey Law <caseyjlaw@gmail.com>
2016-11-01 15:12:11 -0700

    added telcal calibration

---
+++ rfpipe/calibration.py
@@ -0,0 +1,18 @@
+from __future__ import print_function, division, absolute_import
+
+from rtpipe.parsecal import telcal_sol
+
+def apply_telcal(st, segment, data):
+    """ Applies gain calibration from telcal file to input data """
+
+    data2 = data.copy()
+    
+    radec = ()
+    spwind = []
+    calname = ''  # set defaults
+
+    sols = telcal_sol(st.gainfile)
+    sols.set_selection(st.segmenttimes[segment].mean(), st.freq*1e9, st.blarr, calname=calname, pols=st.pols, radec=radec, spwind=spwind)
+    sols.apply(data2)
+
+    return data2
