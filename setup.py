afbbf97969a6a703a0fbb88766643f11c5cd5c57
Casey Law <caseyjlaw@gmail.com>
2017-03-29 15:21:30 -0600

    backing out cython and moving to vysmaw_apps

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
 import numpy, glob
-from Cython.Build import cythonize
-
-extensions = [
-    Extension("*", ["cython/*.pyx"],
-        include_dirs = ["/home/cbe-master/realfast/anaconda/include", "/home/cbe-master/realfast/anaconda/include/python2.7"],
-        libraries = ["vysmaw", "vys", "python2.7"],
-        library_dirs = ["/home/cbe-master/realfast/anaconda/lib/python2.7/site-packages"],)
-]
 
 setup(
     name = 'rfpipe',
@@ -18,7 +10,6 @@ setup(
     url = 'http://github.com/realfastvla/rfpipe',
     packages = find_packages(),        # get all python scripts in realtime
     install_requires=['numpy', 'scipy', 'pwkit', 'sdmpy>=1.35', 'pyfftw', 'click', 'distributed>=1.13', 'attrs', 'future'],
-    ext_modules = cythonize(extensions),
     zip_safe=False,
     classifiers=[
         'Programming Language :: Python :: 2.7'
