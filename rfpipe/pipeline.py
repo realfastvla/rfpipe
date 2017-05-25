7c88cb884fe03afc4a9debead5c9d1edaf52b54f
Casey Law <caseyjlaw@gmail.com>
2017-05-25 15:03:29 -0600

    made pipeline_vys2 to do simple 1 segment pipeline

--- rfpipe/pipeline.py
+++ rfpipe/pipeline.py
@@ -17,11 +17,16 @@ import numpy as np
 
 def pipeline_vys(wait, nsegment=1, nant=3, nspw=1, nchan=64, npol=1, inttime_micros=1e5, host='cbe-node-01', preffile=None, cfile=None):
     """ Start nsegment vysmaw jobs reading a segment each after time wait
+    Mocks metadata.
     """
 
     assert nsegment > 0
 
     cl = distributed.Client('{0}:{1}'.format(host, '8786'))
+    t0 = time.Time.now() + time.TimeDelta(wait, format='sec')
+
+    meta = cl.submit(metadata.mock_metadata, t0.mjd, nant, nspw, nchan, npol, inttime_micros)
+    st = cl.submit(state.State, preffile=preffile, inmeta=meta, inprefs={'nsegment':nsegment})
     data_delayed = [delayed(source.read_vys_seg)(st, seg, cfile=cfile) for seg in range(nsegment)]
 
     data_fut = []
@@ -37,48 +42,36 @@ def pipeline_vys(wait, nsegment=1, nant=3, nspw=1, nchan=64, npol=1, inttime_mic
     return data_fut
 
 
-def pipeline_vys2(wait, nsegment=1, nant=3, nspw=1, nchan=64, npol=1, inttime_micros=1e5, host='cbe-node-01', preffile=None, cfile=None, workers=None):
-    """ Start nsegment vysmaw jobs reading a segment each after time wait
+def pipeline_vys2(st, seg, host='cbe-node-01', cfile=None, workers=None):
+    """ Start pipeline search of a segment by catching vysmaw data.
+    Uses 
     """
 
     assert nsegment > 0
     allow_other_workers = workers != None
 
     cl = distributed.Client('{0}:{1}'.format(host, '8786'))
-    t0 = time.Time.now() + time.TimeDelta(wait, format='sec')
-
-    meta = cl.submit(metadata.mock_metadata, t0.mjd, nant, nspw, nchan, npol, inttime_micros)
-    st = cl.submit(state.State, preffile=preffile, inmeta=meta, inprefs={'nsegment':nsegment})
-    data_delayed = [delayed(source.read_vys_seg)(st, seg, cfile=cfile) for seg in range(nsegment)]
+    data_prep = cl.submit(source.read_vys_seg, st, seg, cfile=cfile)
 
     features = []
     saved = []
     data_fut = []
 
-    while len(data_delayed):
-        if len([df for df in data_fut if not df.done()]) < 2:
-            dd = data_delayed.pop()
-            data_prep = cl.compute(dd)
-            data_fut.append(data_prep)
+    wisdom = cl.submit(search.set_wisdom, st.npixx, st.npixy, pure=True, workers=workers, allow_other_workers=allow_other_workers)
 
-            wisdom = cl.submit(search.set_wisdom, st.npixx, st.npixy, pure=True, workers=workers, allow_other_workers=allow_other_workers)
+    for dmind in range(len(st.dmarr)):
+        delay = cl.submit(util.calc_delay, st.freq, st.freq[-1], st.dmarr[dmind], st.metadata.inttime, pure=True, workers=workers, allow_other_workers=allow_other_workers)
+        data_dm = cl.submit(search.dedisperse, data_prep, delay, pure=True, workers=workers, allow_other_workers=allow_other_workers)
 
-            for dmind in range(len(st.dmarr)):
-                delay = cl.submit(util.calc_delay, st.freq, st.freq[-1], st.dmarr[dmind], st.metadata.inttime, pure=True, workers=workers, allow_other_workers=allow_other_workers)
-                data_dm = cl.submit(search.dedisperse, data_prep, delay, pure=True, workers=workers, allow_other_workers=allow_other_workers)
+        for dtind in range(len(st.dtarr)):
+            uvw = st.get_uvw_segment(segment)
+            ims_thresh = cl.submit(search.resample_image, data_dm, st.dtarr[dtind], uvw, st.freq, st.npixx, st.npixy, st.uvres, st.prefs.sigma_image1, wisdom, pure=True, workers=workers, allow_other_workers=allow_other_workers)
 
-                for dtind in range(len(st.dtarr)):
-                    uvw = st.get_uvw_segment(segment)
-                    ims_thresh = cl.submit(search.resample_image, data_dm, st.dtarr[dtind], uvw, st.freq, st.npixx, st.npixy, st.uvres, st.prefs.sigma_image1, wisdom, pure=True, workers=workers, allow_other_workers=allow_other_workers)
+            feature = cl.submit(search.calc_features, ims_thresh, dmind, st.dtarr[dtind], dtind, segment, st.features, pure=True, workers=workers, allow_other_workers=allow_other_workers)
+            features.append(feature)
 
-                    feature = cl.submit(search.calc_features, ims_thresh, dmind, st.dtarr[dtind], dtind, segment, st.features, pure=True, workers=workers, allow_other_workers=allow_other_workers)
-                    features.append(feature)
-
-            cands = cl.submit(search.collect_cands, features, pure=True, workers=workers, allow_other_workers=allow_other_workers)
-            saved.append(cl.submit(search.save_cands, st, cands, segment, pure=True, workers=workers, allow_other_workers=allow_other_workers))
-
-        else:
-            sleep(0.1)
+    cands = cl.submit(search.collect_cands, features, pure=True, workers=workers, allow_other_workers=allow_other_workers)
+    saved.append(cl.submit(search.save_cands, st, cands, segment, pure=True, workers=workers, allow_other_workers=allow_other_workers))
 
     return saved
 
