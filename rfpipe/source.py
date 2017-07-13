d3cecbefa8076a79baf591fae06af3064f253fc1
Casey Law <caseyjlaw@gmail.com>
2017-07-13 15:40:28 -0700

    pep8 work

--- rfpipe/source.py
+++ rfpipe/source.py
@@ -2,17 +2,17 @@ from __future__ import print_function, division, absolute_import #, unicode_lite
 from builtins import bytes, dict, object, range, map, input#, str # not casa compatible
 from future.utils import itervalues, viewitems, iteritems, listvalues, listitems
 
-import logging
-logger = logging.getLogger(__name__)
-
 import os.path
 from lxml.etree import XMLSyntaxError
 import numpy as np
 import sdmpy
 from astropy import time
+import pwkit.environments.casa.util as casautil
 from rfpipe import util, calibration
 
-import pwkit.environments.casa.util as casautil
+import logging
+logger = logging.getLogger(__name__)
+
 qa = casautil.tools.quanta()
 default_timeout = 10
 
@@ -22,7 +22,7 @@ def data_prep(st, data):
     """
 
     # ** need to make this portable or at least reimplement in rfpipe
-    calibration.apply_telcal(st, data)  
+    calibration.apply_telcal(st, data)
 
     # ** dataflag points to rtpipe for now
     util.dataflag(st, data)
@@ -35,7 +35,8 @@ def data_prep(st, data):
 
 def read_segment(st, segment, cfile=None, timeout=default_timeout):
     """ Read a segment of data and do low-level data preparation.
-    Optionally applies antenna flags, rolls out of order data, and can downsample data in time or frequency.
+    Optionally applies antenna flags, rolls out of order data, and can
+    downsample data in time or frequency.
     cfile and timeout are specific to vys data.
     """
 
@@ -44,7 +45,8 @@ def read_segment(st, segment, cfile=None, timeout=default_timeout):
     elif st.metadata.datasource == 'vys':
         data_read = read_vys_segment(st, segment, cfile=cfile, timeout=timeout)
     else:
-        logger.error('Datasource {0} not recognized.'.format(st.metadata.datasource))
+        logger.error('Datasource {0} not recognized.'
+                     .format(st.metadata.datasource))
 
     # read Flag.xml and apply flags for given ant/time range
     if st.prefs.applyonlineflags:
@@ -53,21 +55,26 @@ def read_segment(st, segment, cfile=None, timeout=default_timeout):
         scan = sdm.scan(st.metadata.scan)
 
         # segment flagged from logical OR from (start, stop) flags
-        t0,t1 = st.segmenttimes[segment]
-        flags = scan.flags([t0,t1]).all(axis=0)  # 0=bad, 1=good. axis=0 is time axis.
+        t0, t1 = st.segmenttimes[segment]
+        # 0=bad, 1=good. axis=0 is time axis.
+        flags = scan.flags([t0, t1]).all(axis=0)
 
         if not flags.all():
-            logger.info('Found antennas to flag in time range {0}-{1} '.format(t0, t1))
-            data_read = np.where(flags[None,:,None, None] == 1, data_read, 0j)
+            logger.info('Found antennas to flag in time range {0}-{1} '
+                        .format(t0, t1))
+            data_read = np.where(flags[None, :, None, None] == 1,
+                                 data_read, 0j)
         else:
-            logger.info('No flagged antennas in time range {0}-{1} '.format(t0, t1))
+            logger.info('No flagged antennas in time range {0}-{1} '
+                        .format(t0, t1))
     else:
         logger.info('Not applying online flags.')
 
     # If spw are rolled, roll them to increasing frequency order
     dfreq = np.array([st.metadata.spw_reffreq[i+1] - st.metadata.spw_reffreq[i]
                       for i in range(len(st.metadata.spw_reffreq)-1)])
-    dfreqneg = [df for df in dfreq if df < 0]     # not a perfect test of permutability!
+    # not a perfect test of permutability!
+    dfreqneg = [df for df in dfreq if df < 0]
 
     if len(dfreqneg) <= 1:
         if len(dfreqneg) == 1:
@@ -83,23 +90,23 @@ def read_segment(st, segment, cfile=None, timeout=default_timeout):
 
     # optionally integrate (downsample)
     if ((st.prefs.read_tdownsample > 1) or (st.prefs.read_fdownsample > 1)):
-        raise NotImplementedError
-
         sh = data_read.shape
         tsize = sh[0]/st.prefs.read_tdownsample
         fsize = sh[2]/st.prefs.read_fdownsample
-        data2 = np.zeros((tsize, sh[1], fsize, sh[3]), dtype='complex64')
+        data_read2 = np.zeros((tsize, sh[1], fsize, sh[3]), dtype='complex64')
         if st.prefs.read_tdownsample > 1:
-            logger.info('Downsampling in time by {0}'.format(st.prefs.read_tdownsample))
+            logger.info('Downsampling in time by {0}'
+                        .format(st.prefs.read_tdownsample))
             for i in range(tsize):
-                data2[i] = data[
+                data_read2[i] = data_read[
                     i*st.prefs.read_tdownsample:(i+1)*st.prefs.read_tdownsample].mean(axis=0)
         if st.prefs.read_fdownsample > 1:
-            logger.info('Downsampling in frequency by {0}'.format(st.prefs.read_fdownsample))
+            logger.info('Downsampling in frequency by {0}'
+                        .format(st.prefs.read_fdownsample))
             for i in range(fsize):
-                data2[:, :, i, :] = data[
+                data_read2[:, :, i, :] = data_read[
                     :, :, i * st.prefs.read_fdownsample:(i+1)*st.prefs.read_fdownsample].mean(axis=2)
-        data = data2
+        data_read = data_read2
 
 #    takepol = [st.metadata.pols_orig.index(pol) for pol in st.pols]
 #    logger.debug('Selecting pols {0}'.format(st.pols))
@@ -108,9 +115,10 @@ def read_segment(st, segment, cfile=None, timeout=default_timeout):
     return data_read.take(st.chans, axis=2)
 
 
-def read_vys_seg(st, seg, cfile=None, timeout=default_timeout):
+def read_vys_segment(st, seg, cfile=None, timeout=default_timeout):
     """ Read segment seg defined by state st from vys stream.
-    Uses vysmaw application timefilter to receive multicast messages and pull spectra on the CBE.
+    Uses vysmaw application timefilter to receive multicast messages and pull
+    spectra on the CBE.
     """
 
     # TODO: support for time downsampling
@@ -118,33 +126,45 @@ def read_vys_seg(st, seg, cfile=None, timeout=default_timeout):
     try:
         import vysmaw_reader
     except ImportError:
-        logger.error('ImportError for vysmaw app. Need vysmaw to consume vys data.')
+        logger.error('ImportError for vysmaw app. Need vysmaw to '
+                     'consume vys data.')
 
     t0 = time.Time(st.segmenttimes[seg][0], format='mjd', precision=9).unix
     t1 = time.Time(st.segmenttimes[seg][1], format='mjd', precision=9).unix
-    logger.info('Reading %d ints of size %f s from %d - %d unix seconds' % (st.readints, st.metadata.inttime, t0, t1))
+    logger.info('Reading {0} ints of size {1} s from {2} - {3} unix seconds'
+                .format(st.readints, st.metadata.inttime, t0, t1))
 
-    data = np.empty( (st.readints, st.metadata.nbl_orig, st.metadata.nchan_orig, st.metadata.npol_orig), dtype='complex64', order='C')
+    data = np.empty((st.readints, st.metadata.nbl_orig,
+                     st.metadata.nchan_orig, st.metadata.npol_orig),
+                    dtype='complex64', order='C')
     with vysmaw_reader.Reader(t0, t1, cfile=cfile) as reader:
-        data[:] = reader.readwindow(nant=st.nants, nspw=st.nspw, nchan=st.metadata.spw_nchan[0], npol=st.npol, 
-                                    inttime_micros=st.metadata.inttime*1e6, timeout=timeout, excludeants=st.prefs.excludeants)
+        data[:] = reader.readwindow(nant=st.nants, nspw=st.nspw,
+                                    nchan=st.metadata.spw_nchan[0],
+                                    npol=st.npol,
+                                    inttime_micros=st.metadata.inttime*1e6,
+                                    timeout=timeout,
+                                    excludeants=st.prefs.excludeants)
 
     return data
 
 
 def read_bdf_segment(st, segment):
-    """ Uses sdmpy to reads bdf (sdm) format data into numpy array in given segment.
+    """ Uses sdmpy to reads bdf (sdm) format data into numpy array in given
+    segment.
     """
 
-    assert segment < st.nsegment, 'segment {0} is too big for nsegment {1}' % (segment, st.nsegment)
+    assert segment < st.nsegment, ('segment {0} is too big for nsegment {1}'
+                                   .format(segment, st.nsegment))
 
     # define integration range
-    nskip = (24*3600*(st.segmenttimes[segment, 0] - st.metadata.starttime_mjd) / st.metadata.inttime).astype(int)
-    logger.info('Reading scan {0}, segment {1}/{2}, times {3} to {4}'.format(st.metadata.scan, segment, len(st.segmenttimes)-1,
-                                                                             qa.time(qa.quantity(st.segmenttimes[segment, 0], 'd'),
-                                                                                     form=['hms'], prec=9)[0],
-                                                                             qa.time(qa.quantity(st.segmenttimes[segment, 1], 'd'),
-                                                                                     form=['hms'], prec=9)[0]))
+    nskip = (24*3600*(st.segmenttimes[segment, 0]
+                      - st.metadata.starttime_mjd)/st.metadata.inttime).astype(int)
+    logger.info('Reading scan {0}, segment {1}/{2}, times {3} to {4}'
+                .format(st.metadata.scan, segment, len(st.segmenttimes)-1,
+                        qa.time(qa.quantity(st.segmenttimes[segment, 0], 'd'),
+                                form=['hms'], prec=9)[0],
+                        qa.time(qa.quantity(st.segmenttimes[segment, 1], 'd'),
+                                form=['hms'], prec=9)[0]))
     data = read_bdf(st, nskip=nskip).astype('complex64')
 
     return data
@@ -155,14 +175,17 @@ def read_bdf(st, nskip=0):
     readints=0 will read all of bdf (skipping nskip).
     """
 
-    assert os.path.exists(st.metadata.filename), 'sdmfile {0} does not exist'.format(st.metadata.filename)
-    assert st.metadata.bdfstr, 'bdfstr not defined for scan {0}'.format(st.metadata.scan)
+    assert os.path.exists(st.metadata.filename), ('sdmfile {0} does not exist'
+                                                  .format(st.metadata.filename))
+    assert st.metadata.bdfstr, ('bdfstr not defined for scan {0}'
+                                .format(st.metadata.scan))
 
     logger.info('Reading %d ints starting at int %d' % (st.readints, nskip))
     sdm = getsdm(st.metadata.filename, bdfdir=st.metadata.bdfdir)
     scan = sdm.scan(st.metadata.scan)
 
-    data = np.empty( (st.readints, st.metadata.nbl_orig, st.metadata.nchan_orig, st.metadata.npol_orig), dtype='complex64', order='C')
+    data = np.empty((st.readints, st.metadata.nbl_orig, st.metadata.nchan_orig,
+                     st.metadata.npol_orig), dtype='complex64', order='C')
     data[:] = scan.bdf.get_data(trange=[nskip, nskip+st.readints]).reshape(data.shape)
 
     return data
@@ -178,7 +201,8 @@ def sdm_sources(sdmname):
         src = str(row.fieldName)
         sourcenum = int(row.sourceId)
         direction = str(row.referenceDir)
-        (ra,dec) = [float(val) for val in direction.split(' ')[3:]]  # skip first two values in string
+        # skip first two values in string
+        (ra, dec) = [float(val) for val in direction.split(' ')[3:]]
 
         sourcedict[sourcenum] = {}
         sourcedict[sourcenum]['source'] = src
