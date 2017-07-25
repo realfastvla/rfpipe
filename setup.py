5c5f47c8ef440f53e4ca7f99ccf0be3bf22ca729
Casey Law <caseyjlaw@gmail.com>
2017-07-25 14:08:19 -0700

    adding numba as dep

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import numpy, glob
 exec(open('rfpipe/version.py').read())  # set __version__
 
 setup(
-    name = 'rfpipe',
-    description = 'realfast pipeline',
-    author = 'Casey Law',
-    author_email = 'caseyjlaw@gmail.com',
-    version = __version__,
-    url = 'http://github.com/realfastvla/rfpipe',
-    packages = find_packages(),        # get all python scripts in realtime
-    install_requires=['numpy', 'scipy', 'pwkit', 'sdmpy>=1.35', 'pyfftw', 'click', 'dask',
-                      'distributed>=1.13', 'attrs', 'future', 'astropy', 'pyyaml', 'lxml',
-                      'rtpipe'],
+    name='rfpipe',
+    description='realfast pipeline',
+    author='Casey Law',
+    author_email='caseyjlaw@gmail.com',
+    version=__version__,
+    url='http://github.com/realfastvla/rfpipe',
+    packages=find_packages(),        # get all python scripts in realtime
+    install_requires=['numpy', 'scipy', 'pwkit', 'sdmpy>=1.35', 'pyfftw',
+                      'click', 'dask', 'distributed>=1.13', 'attrs', 'future',
+                      'astropy', 'pyyaml', 'lxml', 'numba', 'rtpipe'],
     zip_safe=False,
     classifiers=[
         'Programming Language :: Python :: 2.7'
