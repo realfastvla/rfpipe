1d6ca278842f514cba0274e8b8939fb5e4b0baac
Casey Law <caseyjlaw@gmail.com>
2017-08-02 17:42:57 -0600

    fixed path to telcal file

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -63,7 +63,9 @@ def test_sdmmeta():
 
 
 def test_sdm():
-    sdmfile = os.path.join(_install_dir, 'data/16A-459_TEST_1hr_000.57633.66130137732.scan7.cut1')
-    inprefs = {'gainfile': '16A-459_TEST_1hr_000.57633.66130137732.scan7.cut1.GN'}
+    sdmfile = os.path.join(_install_dir,
+                           'data/16A-459_TEST_1hr_000.57633.66130137732.scan7.cut1')
+    inprefs = {'gainfile': os.path.join(_install_dir,
+                                        '16A-459_TEST_1hr_000.57633.66130137732.scan7.cut1.GN')}
     state = rfpipe.state.State(sdmfile=sdmfile, sdmscan=7, inprefs=inprefs)
     res = rfpipe.pipeline.pipeline_seg(state, 0)
