a49d437f6cd821001d276277a23b03acaddb2c02
CBE Transient (sspolaor) <realfast@cbe-node-01.evla.nrao.edu>
2018-02-04 16:47:07 -0700

    simple flagging tests working

--- tests/flagging_olympics.py
+++ tests/flagging_olympics.py
@@ -10,18 +10,18 @@ import numpy as np
 # success means that a standard set of simulated transients is found
 # in four set ups: no timesub/flagging, no timesub, no flagging, with both.
 
-transients = [(0, 0, 0, 5e-3, 0.1, 0., 0.), (0, 30, 25, 10e-3, 1., 0.001, 0.001)]
+transients = [(0, 10, 0, 5e-3, 0.1, -0.001, -0.001), (0, 30, 50, 5e-3, 0.1, 0.001, 0.001)]
 
-inprefs = [{'maxdm': 100, 'dtarr': [1, 2, 4, 8], 'npix_max': 1024,
+inprefs = [{'maxdm': 100, 'dtarr': [1], 'npix_max': 1024,
             'simulated_transient': transients,
             'timesub': None, 'flaglist': []},
-           {'maxdm': 100, 'dtarr': [1, 2, 4, 8], 'npix_max': 1024,
+           {'maxdm': 100, 'dtarr': [1], 'npix_max': 1024,
             'simulated_transient': transients,
             'timesub': 'mean', 'flaglist': []},
-           {'maxdm': 100, 'dtarr': [1, 2, 4, 8], 'npix_max': 1024,
+           {'maxdm': 100, 'dtarr': [1], 'npix_max': 1024,
             'simulated_transient': transients,
             'timesub': None},
-           {'maxdm': 100, 'dtarr': [1, 2, 4, 8], 'npix_max': 1024,
+           {'maxdm': 100, 'dtarr': [1], 'npix_max': 1024,
             'simulated_transient': transients,
             'timesub': 'mean'}]
 
@@ -36,7 +36,7 @@ def st(request):
 
 @pytest.fixture(scope="module")
 def data(st):
-    data_read = rfpipe.source.read_segment(st, 0)
+    return rfpipe.source.read_segment(st, 0)
 
 
 @pytest.fixture(scope="module")
@@ -46,14 +46,15 @@ def data_prep(st, data):
 
 @pytest.fixture(scope="module")
 def data_prep_rfi(st, data):
-    data[10] += 0.01
+    data[5] += 0.01
     data[:, :, 30] += 0.01
-    data[22, :, 100:110, 0] += np.random.normal(0, 0.1, (st.nchan,))
-    data[100, :, 22:30, 1] += np.random.normal(0, 0.1, (st.nchan,))
+    for i in range(110, 120):
+        data[22, :, i, 0] += np.random.normal(0, 0.1, (st.nbl,))
     return rfpipe.source.data_prep(st, 0, data)
 
 
 def test_cuda(st, data_prep):
+    rfgpu = pytest.importorskip('rfgpu')
     for dmind in range(len(st.dmarr)):
         canddatalist = rfpipe.search.dedisperse_image_cuda(st, 0, data_prep, dmind)
         assert len(canddatalist)
@@ -67,6 +68,7 @@ def test_fftw(st, data_prep):
 
 
 def test_cuda_rfi(st, data_prep_rfi):
+    rfgpu = pytest.importorskip('rfgpu')
     for dmind in range(len(st.dmarr)):
         canddatalist = rfpipe.search.dedisperse_image_cuda(st, 0, data_prep_rfi, dmind)
         assert len(canddatalist)
