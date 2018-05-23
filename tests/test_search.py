abe5e1ce546ea2d16cdfaad32a1358af380ef5fe
Casey Law <caseyjlaw@gmail.com>
2018-05-23 13:11:02 -0600

    further test shrinkage for nojit case

--- tests/test_search.py
+++ tests/test_search.py
@@ -14,7 +14,7 @@ def st():
         inprefs = {'flaglist': [], 'npix_max': 128, 'uvres': 500, 'nthread': 1,
                    'fftmode': 'fftw'}
         t0 = time.Time.now().mjd
-        meta = rfpipe.metadata.mock_metadata(t0, t0+0.05/(24*3600), 20, 4, 32*4,
+        meta = rfpipe.metadata.mock_metadata(t0, t0+0.05/(24*3600), 10, 4, 32*4,
                                              2, 5e3, datasource='sim')
         return rfpipe.state.State(inmeta=meta, inprefs=inprefs)
 
