1fae023176b2941f87aa7bb667ca7f914e9db7de
Casey Law <caseyjlaw@gmail.com>
2018-06-07 17:42:27 -0600

    little logging improvement

--- rfpipe/calibration.py
+++ rfpipe/calibration.py
@@ -51,7 +51,8 @@ def apply_telcal(st, data, threshold=1/50., onlycomplete=True, sign=+1):
                                                         nchan[0], sign=sign),
                                           copy=False)
                 blinds, chans, pols = np.where(gaindelay == 0)
-                print('Missed {0} solutions with bls {1}'.format(len(blinds), st.blarr[np.unique(blinds)]))
+                if len(blinds):
+                    logger.info('Missed {0} solutions with bls {1}'.format(len(blinds), st.blarr[np.unique(blinds)]))
             else:
                 gaindelay = np.zeros_like(data)
 
