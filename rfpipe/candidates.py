fixing tests for new dedispersion option. fixing plot integraiton for averaged spectrum

rfpipe/candidates.py
@@ -1200,7 +1200,7 @@ def candplot(canddatalist, snrs=None, cluster=None, outname=''):
         ax_dynsp3.xaxis.set_label_position('top')
 
         # plot stokes I spectrum of the candidate pulse from middle integration
-        ax_sp.plot(dd2avgcrop[:, canddata.integration_rel//2]/2.,
+        ax_sp.plot(dd2avgcrop[:, canddata.integration_rel]/2.,
                    list(range(len(dd2avgcrop))), 'k.')
         ax_sp.plot(np.zeros(len(dd2avgcrop)), list(range(len(dd2avgcrop))),
                    'r:')

tests/test_search.py
@@ -60,7 +60,8 @@ def test_dmresample_single(st, data):
 
     data1 = rfpipe.search.dedisperse(data, delay, parallel=False)
     data2 = rfpipe.search.resample(data1, dt, parallel=False)
-    data3 = rfpipe.search.dedisperseresample(data, delay, dt, parallel=False)
+    data3 = rfpipe.search.dedisperseresample(data, delay, dt, parallel=False,
+                                             resamplefirst=False)
     assert np.allclose(data3, data2)
 
 
@@ -72,7 +73,8 @@ def test_dmresample_multi1(st, data):
 
     data1 = rfpipe.search.dedisperse(data, delay, parallel=True)
     data2 = rfpipe.search.resample(data1, dt, parallel=True)
-    data3 = rfpipe.search.dedisperseresample(data, delay, dt, parallel=True)
+    data3 = rfpipe.search.dedisperseresample(data, delay, dt, parallel=True,
+                                             resamplefirst=False)
     assert np.allclose(data3, data2)
 
 
@@ -84,7 +86,8 @@ def test_dmresample_multi2(st, data):
 
     data1 = rfpipe.search.dedisperse(data, delay, parallel=True)
     data2 = rfpipe.search.resample(data1, dt, parallel=True)
-    data3 = rfpipe.search.dedisperseresample(data, delay, dt, parallel=True)
+    data3 = rfpipe.search.dedisperseresample(data, delay, dt, parallel=True,
+                                             resamplefirst=False)
     assert np.allclose(data3, data2)
 
 