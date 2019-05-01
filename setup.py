afd21909ea7ca2cb6822979e52a8d80ea285b1ff
Casey Law <caseyjlaw@gmail.com>
2019-04-30 21:26:31 -0600

    rolling hdbscan back again

--- setup.py
+++ setup.py
@@ -19,9 +19,8 @@ AUTHOR = 'Casey Law'
 
 # What packages are required for this module to be executed?
 REQUIRED = ['numpy', 'scipy', 'pwkit', 'sdmpy', 'pyfftw', 'bokeh', 'cython', 'scikit-learn',
-            'attrs', 'future', 'astropy', 'pyyaml', 'numba', 'fuzzywuzzy', 'hdbscan',
+            'attrs', 'future', 'astropy', 'pyyaml', 'numba', 'fuzzywuzzy', 'hdbscan<0.8.19',
             'matplotlib', 'kalman_detector']
-# 'hdbscan<0.8.19', ?
             # optional 'rfgpu', 'distributed'
 
 # The rest you shouldn't have to touch too much :)
