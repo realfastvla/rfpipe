6344342d2251d3b60f409a9def032f0c9bded176
Casey Law <caseyjlaw@gmail.com>
2019-05-02 14:09:13 -0600

    getting timestamps back on logging

--- rfpipe/__init__.py
+++ rfpipe/__init__.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import
 
 __all__ = ['state', 'preferences', 'metadata', 'source', 'pipeline', 'reproduce']
-          # 'candidates', 'flagging', 'calibration', 'util', 'search'
+          # 'candidates', 'flagging', 'calibration', 'util', 'search'  # these are slow to import
 
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 from rfpipe import *
 from rfpipe.version import __version__
-import logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
