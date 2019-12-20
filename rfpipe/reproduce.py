2e8365633df657e58b06f141da4db008d97c9d5f
Casey Law <caseyjlaw@gmail.com>
2019-12-20 12:12:19 -0700

    raising an error if no bdf available for an SDM (again)

--- rfpipe/reproduce.py
+++ rfpipe/reproduce.py
@@ -289,9 +289,9 @@ def refine_sdm(sdmname, dm, preffile='realfast.yml', gainpath='/home/mchammer/ev
             logger.warn("Could not generate state with 2x images. Trying with original image size...")
             prefs['npix_max'] = min(npix_max, npix_max_orig)
             st = state.State(sdmfile=sdmname, sdmscan=1, inprefs=prefs, preffile=preffile, name='NRAOdefault'+band, bdfdir=bdfdir, showsummary=False)
-        except FileNotFoundError:
-            logger.warn("No BDF found for sdmname {0}".format(sdnmame))
-            return cc
+    except FileNotFoundError:
+        logger.warn("No BDF found for sdmname {0}".format(sdnmame))
+        return cc
 
     st.prefs.dmarr = sorted([dm] + [dm0 for dm0 in st.dmarr if (dm0 == 0 or dm0 > dm-ddm)])  # remove superfluous dms, enforce orig dm
     st.clearcache()
