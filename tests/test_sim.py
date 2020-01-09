2c96c665af503350ed2e173be4a5192a4e471ff1
Kshitij Aggarwal <ka0064@mix.wvu.edu>
2020-01-09 01:08:24 -0500

    added tests for injecting wider (dt>2) transients

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -6,7 +6,7 @@ from io import open
 import rfpipe, rfpipe.candidates
 import pytest
 from astropy import time
-from numpy import degrees, nan
+from numpy import degrees, nan, argmax
 
 tparams = [(0, 0, 0, 5e-3, 0.3, 0.0001, 0.0),]
 # simulate no flag, transient/no flag, transient/flag
@@ -111,3 +111,33 @@ def test_phasecenter_detection():
     cc = rfpipe.pipeline.pipeline_scan(st)
     assert all(cc.array['l1'] == 0.)
     assert all(cc.array['m1'] == 0.)
+
+def test_wide_transient():
+    print("Try injecting a transient of width 40ms at integration 8")
+    inprefs = {'simulated_transient': [(0, 8, 0, 40e-3, 0.3, 0., 0.)],
+           'dmarr': [0], 'dtarr': [1,2,4,8], 'timesub': None, 'fftmode': 'fftw', 'searchtype': 'image',
+           'sigma_image1': 10, 'flaglist': [], 'uvres': 60, 'npix_max': 128, 'max_candfrac': 0}
+
+    t0 = time.Time.now().mjd
+    meta = rfpipe.metadata.mock_metadata(t0, t0+0.1/(24*3600), 20, 4, 32*4, 2,
+                                         5e3, scan=1, datasource='sim',
+                                         antconfig='D')
+    st = rfpipe.state.State(inmeta=meta, inprefs=inprefs)
+    cc = rfpipe.pipeline.pipeline_scan(st)
+    ind = argmax(cc.array['snr1'])
+    assert cc.array['dtind'][ind] == 3
+    assert cc.array['integration'][ind]*2**cc.array['dtind'][ind] == 8    
+    
+    print("Try injecting a transient of width 20ms at integration 8")
+    inprefs['simulated_transient'] = [(0, 8, 0, 20e-3, 0.3, 0., 0.)]
+    t0 = time.Time.now().mjd
+    meta = rfpipe.metadata.mock_metadata(t0, t0+0.1/(24*3600), 20, 4, 32*4, 2,
+                                         5e3, scan=1, datasource='sim',
+                                         antconfig='D')
+
+    st = rfpipe.state.State(inmeta=meta, inprefs=inprefs)
+
+    cc = rfpipe.pipeline.pipeline_scan(st)
+    ind = argmax(cc.array['snr1'])
+    assert cc.array['dtind'][ind] == 2
+    assert cc.array['integration'][ind]*2**cc.array['dtind'][ind] == 8    
