6689923ed66136822130c30788ef63056b3e6172
Casey Law <caseyjlaw@gmail.com>
2020-04-23 16:26:18 -0600

    making python3 only

--- setup.py
+++ setup.py
@@ -109,7 +109,6 @@ setup(
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
     ],
     # $ setup.py publish support. 
